a0a75d945e35dcbbef784d44cc90c878dc604efc
Andrei Tognolo <andreitognolo@gmail.com>
2016-07-28 15:16:09 -0300

    Update 0-init.sh

--- bin/dev/snap-ci/master/0-init.sh
+++ bin/dev/snap-ci/master/0-init.sh
@@ -1,9 +1,3 @@
 #!/usr/bin/env bash
 
-ARTIFACT_DIR="${SNAP_WORKING_DIR}/snap-artifacts"
-KIEKER_DIR="${ARTIFACT_DIR}/kieker"
-TMP_DIR=/tmp/kieker
-
-cp -r ${SNAP_WORKING_DIR}/. ${TMP_DIR}
-mkdir -p ${ARTIFACT_DIR}/
-mv ${TMP_DIR}/ ${ARTIFACT_DIR}
+echo "No operation :)"
